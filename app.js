069e7a0a4caa96ce58e639026f80759282fd54a9
Ivan <ivanglzr01@gmail.com>
2024-08-25 14:36:21 +0200

    cors middleware implemented in app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
 
+import { cors } from "./src/middlewares/cors.js";
+
 import userRouter from "./src/routes/user.js";
 
 const app = express();
 
 app.disable("x-powered-by");
 app.use(express.json());
+app.use(cors);
 
 app.use(userRouter);
 
