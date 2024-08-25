b94034f1af48c1302310e2b6e172db9deb696716
Ivan <ivanglzr01@gmail.com>
2024-08-25 16:21:00 +0200

    order router implemented in user router

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,11 +4,15 @@ import { authenticateUser } from "../middlewares/authenticateUser.js";
 
 import { loginUser, postUser } from "../controllers/user.js";
 
+import orderRouter from "./order.js";
+
 const userRouter = Router();
 
 userRouter.post("/login", loginUser);
 userRouter.post("/register", postUser);
 
 userRouter.use("/user", authenticateUser);
+
+userRouter.use("/user", orderRouter);
 
 export default userRouter;
