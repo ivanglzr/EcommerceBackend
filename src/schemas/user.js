bda1d2224ddc69a207c95ba8e2a8250b89613530
Ivan <ivanglzr01@gmail.com>
2024-08-25 14:56:55 +0200

    loginSchema created

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -6,10 +6,19 @@ const userSchema = z.object({
   password: z.string().min(8),
 });
 
+const loginSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
+
 export function validateUser(user) {
   return userSchema.safeParse(user);
 }
 
 export function validatePartialUser(partialUser) {
   return userSchema.partial().safeParse(partialUser);
+}
+
+export function validateLogin(login) {
+  return loginSchema.safeParse(login);
 }
