64e3ea5d265640d1b8542dc6d0cfed713e55fa35
Ivan <ivanglzr01@gmail.com>
2024-08-25 14:57:19 +0200

    loginUser controller created and implemented in user routes

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,10 +1,68 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
 
-import { validateUser } from "../schemas/user.js";
+import { validateUser, validateLogin } from "../schemas/user.js";
 
 import { SALT_ROUNDS, statusMessages } from "../config.js";
+
+export async function loginUser(req, res) {
+  const { data, error } = validateLogin(req.body);
+
+  if (error) {
+    return res.status(422).json({
+      status: statusMessages.error,
+      message: "Data isn't valid",
+    });
+  }
+
+  try {
+    const { email, password } = data;
+
+    const userInDb = await User.findOne({ email });
+
+    if (!userInDb) {
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "User not found",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, userInDb.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        status: statusMessages.error,
+        message: "Unauthorized",
+      });
+    }
+
+    const token = jwt.sign({ id: userInDb._id }, process.env.SECRET_KEY, {
+      algorithm: "HS512",
+      expiresIn: "1h",
+    });
+
+    return res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 60 * 60 * 1000,
+      })
+      .json({
+        status: statusMessages.success,
+        message: "Authorized",
+      });
+  } catch (err) {
+    console.error(err);
+
+    return res.status(500).json({
+      status: statusMessages.error,
+      message: "An error ocurred while logging in",
+    });
+  }
+}
 
 export async function postUser(req, res) {
   const { data, error } = validateUser(req.body);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { postUser } from "../controllers/user.js";
+import { loginUser, postUser } from "../controllers/user.js";
 
 const userRouter = Router();
 
+userRouter.post("/login", loginUser);
 userRouter.post("/register", postUser);
 
 export default userRouter;
