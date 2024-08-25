b535203f827dabd71254e52ea732f6ef550f34a9
Ivan <ivanglzr01@gmail.com>
2024-08-25 14:39:17 +0200

    cookieParser middleware in app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { cors } from "./src/middlewares/cors.js";
+import cookieParser from "cookie-parser";
 
 import userRouter from "./src/routes/user.js";
 
@@ -9,6 +10,7 @@ const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
 app.use(cors);
+app.use(cookieParser());
 
 app.use(userRouter);
 
