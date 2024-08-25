acc9cb0ce7274acf0bf75383503545e2d7c9242a
Ivan <ivanglzr01@gmail.com>
2024-08-25 15:04:29 +0200

    authenticate user implemented in user router

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
+import { authenticateUser } from "../middlewares/authenticateUser.js";
+
 import { loginUser, postUser } from "../controllers/user.js";
 
 const userRouter = Router();
 
 userRouter.post("/login", loginUser);
 userRouter.post("/register", postUser);
+
+userRouter.use("/user", authenticateUser);
 
 export default userRouter;
