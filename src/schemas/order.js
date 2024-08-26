1eb234cf2bb5dbe7ae276dc1bc2720a8726ae40e
Ivan <ivanglzr01@gmail.com>
2024-08-26 16:36:57 +0200

    order schema has more strict validations

--- src/schemas/order.js
+++ src/schemas/order.js
@@ -2,14 +2,16 @@ import { z } from "zod";
 import { orderStatus } from "../config.js";
 
 const orderSchema = z.object({
-  products: z.array(
-    z.object({
-      productId: z.string().length(24, "Id must be 24 characters long"),
-      quantity: z.number().optional(),
-    })
-  ),
+  products: z
+    .array(
+      z.object({
+        productId: z.string().length(24, "Id must be 24 characters long"),
+        quantity: z.number().optional(),
+      })
+    )
+    .nonempty("There must be atleast 1 product"),
   status: z.enum(orderStatus).optional(),
-  address: z.string(),
+  address: z.string().min(8, "Address is too short"),
 });
 
 export function validateOrder(order) {
