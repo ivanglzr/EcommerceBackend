57b7d24c7cab1096ed0aff3f32c570f0b541ef6d
Ivan <ivanglzr01@gmail.com>
2024-08-26 16:04:39 +0200

    controllers implemented into product router

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 
+import { validateProductId } from "../middlewares/validateProductId.js";
 import { authenticateAdmin } from "../middlewares/authenticateAdmin.js";
 
-import { postProduct } from "../controllers/product.js";
+import {
+  getProduct,
+  getProducts,
+  postProduct,
+} from "../controllers/product.js";
 
 const productRouter = Router();
+
+productRouter.get("/", getProducts);
+productRouter.get("/:productId", validateProductId, getProduct);
 
 productRouter.use("/", authenticateAdmin);
 
