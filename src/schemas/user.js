5d41f3a4d895f08fe047c6f7dfa40db3ac55b79d
Ivan <ivanglzr01@gmail.com>
2024-08-26 16:13:41 +0200

    error messages in user schemas

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 const userSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  password: z.string().min(8),
+  name: z.string().min(2, "Name is too short"),
+  email: z.string().email("Email isn't valid"),
+  password: z.string().min(8, "Password is too short"),
 });
 
 const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
+  email: z.string().email("Email isn't valid"),
+  password: z.string().min(8, "Password is too short"),
 });
 
 export function validateUser(user) {
