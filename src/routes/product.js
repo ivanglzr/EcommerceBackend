c83d436b9dc4a40d2158979a31dc8c7955866f96
Ivan <ivanglzr01@gmail.com>
2024-08-27 13:20:39 +0200

    multer config

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,3 +1,6 @@
+import multer from "multer";
+import path from "node:path";
+
 import { Router } from "express";
 
 import { validateProductId } from "../middlewares/validateProductId.js";
@@ -8,6 +11,7 @@ import {
   getProducts,
   postProduct,
   putProduct,
+  uploadImage,
 } from "../controllers/product.js";
 
 const productRouter = Router();
@@ -19,5 +23,22 @@ productRouter.use("/", authenticateAdmin);
 
 productRouter.post("/", postProduct);
 productRouter.put("/:productId", validateProductId, putProduct);
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const name = path.basename(file.originalname, ext);
+
+    cb(null, `${name}-${Date.now()}${ext}`);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
 
 export default productRouter;
