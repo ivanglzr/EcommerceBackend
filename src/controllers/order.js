1723c4c663504cb4260493d331f2407cecb7f0b1
Ivan <ivanglzr01@gmail.com>
2024-08-27 21:29:24 +0200

    getOrder methods

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -7,6 +7,75 @@ import { validateOrder } from "../schemas/order.js";
 import Order from "../models/order.js";
 import Product from "../models/product.js";
 
+export async function getOrder(req, res) {
+  const { orderId } = req.params;
+  const id = req.session;
+
+  try {
+    const order = await Order.findOne({ _id: orderId, userId: id });
+
+    if (!order) {
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "Order not found",
+      });
+    }
+
+    const parsedOrder = {
+      _id: order._id,
+      products: order.products,
+      status: order.status,
+      address: order.address,
+      orderedAt: order.orderedAt,
+    };
+
+    return res.json({
+      status: statusMessages.success,
+      message: "Order found",
+      order: parsedOrder,
+    });
+  } catch (_) {
+    return res.status(500).json({
+      status: statusMessages.error,
+      message: "An error ocurred while finding the order",
+    });
+  }
+}
+
+export async function getOrders(req, res) {
+  const id = req.session;
+
+  try {
+    const orders = await Order.find({ userId: id });
+
+    const message =
+      orders.length === 0 ? "User doesn't have any orders" : "Orders found";
+
+    const parsedOrders = orders.map((order) => {
+      const parsedOrder = {
+        _id: order._id,
+        products: order.products,
+        status: order.status,
+        address: order.address,
+        orderedAt: order.orderedAt,
+      };
+
+      return parsedOrder;
+    });
+
+    return res.json({
+      status: statusMessages.success,
+      message,
+      orders: parsedOrders,
+    });
+  } catch (_) {
+    return res.status(500).json({
+      status: statusMessages.error,
+      message: "An error ocurred while finding the orders",
+    });
+  }
+}
+
 export async function postOrder(req, res) {
   const { data, error } = validateOrder(req.body);
 
