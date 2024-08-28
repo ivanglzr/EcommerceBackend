94e3ddc53feacce759e38d490f563afebf6b8090
Ivan <ivanglzr01@gmail.com>
2024-08-28 14:08:29 +0200

    remove status from schema and add a min to the quantity

--- src/schemas/order.js
+++ src/schemas/order.js
@@ -1,16 +1,17 @@
 import { z } from "zod";
-import { orderStatus } from "../config.js";
 
 const orderSchema = z.object({
   products: z
     .array(
       z.object({
         productId: z.string().length(24, "Id must be 24 characters long"),
-        quantity: z.number().optional(),
+        quantity: z
+          .number()
+          .min(1, "Quantity must be greater than 0")
+          .optional(),
       })
     )
     .nonempty("There must be atleast 1 product"),
-  status: z.enum(orderStatus).optional(),
   address: z.string().min(8, "Address is too short"),
 });
 
