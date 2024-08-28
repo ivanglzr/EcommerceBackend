84e14e0914df81eff3347c675c0df300a6a726eb
Ivan <ivanglzr01@gmail.com>
2024-08-28 14:16:53 +0200

    putOrder controller created

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,6 +1,6 @@
 import { Types } from "mongoose";
 
-import { statusMessages } from "../config.js";
+import { orderStatus, statusMessages } from "../config.js";
 
 import { validateOrder } from "../schemas/order.js";
 
@@ -110,6 +110,7 @@ export async function postOrder(req, res) {
     const order = new Order({
       ...data,
       products,
+      status: orderStatus.pending,
       userId: id,
     });
 
@@ -118,6 +119,64 @@ export async function postOrder(req, res) {
     return res.status(201).json({
       status: statusMessages.success,
       message: "Order created",
+    });
+  } catch (_) {
+    return res.status(500).json({
+      status: statusMessages.error,
+      message: "An error ocurred while creating the order",
+    });
+  }
+}
+
+export async function putOrder(req, res) {
+  const { data, error } = validateOrder(req.body);
+
+  if (error) {
+    return res.status(422).json({
+      status: statusMessages.error,
+      message: error.errors[0].message,
+    });
+  }
+
+  const id = req.session;
+  const { orderId } = req.params;
+
+  try {
+    const productIds = data.products.map(({ productId }) =>
+      Types.ObjectId.createFromHexString(productId)
+    );
+
+    const existingProducts = await Product.find({ _id: { $in: productIds } });
+
+    if (existingProducts.length !== productIds.length) {
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "Some products don't exist",
+      });
+    }
+
+    const products = data.products.map(({ productId, quantity }) => ({
+      productId: Types.ObjectId.createFromHexString(productId),
+      quantity: quantity,
+    }));
+
+    const orderData = { ...data, products };
+
+    const order = await Order.findOneAndUpdate(
+      { userId: id, _id: orderId },
+      orderData
+    );
+
+    if (!order) {
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "Order not found",
+      });
+    }
+
+    return res.status(200).json({
+      status: statusMessages.success,
+      message: "Order updated",
     });
   } catch (_) {
     return res.status(500).json({
