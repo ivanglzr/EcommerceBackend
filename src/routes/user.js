414430479b5245b1a49239f87632002bb40bd051
Ivan <ivanglzr01@gmail.com>
2024-08-29 14:21:46 +0200

    cartRouter implemented in userRouter

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,7 @@ import { authenticateUser } from "../middlewares/authenticateUser.js";
 import { loginUser, postUser } from "../controllers/user.js";
 
 import orderRouter from "./order.js";
+import cartRouter from "./cart.js";
 
 const userRouter = Router();
 
@@ -14,5 +15,6 @@ userRouter.post("/register", postUser);
 userRouter.use("/", authenticateUser);
 
 userRouter.use("/order", orderRouter);
+userRouter.use("/cart", cartRouter);
 
 export default userRouter;
