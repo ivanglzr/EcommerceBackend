f8ea47c53572ca69ad113af2d4e48a9deb059633
Ivan <ivanglzr01@gmail.com>
2024-08-29 14:21:00 +0200

    putCart and in getCart send the crat without the userId

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,6 +1,11 @@
+import { Types } from "mongoose";
+
 import Cart from "../models/cart.js";
+import Product from "../models/product.js";
 
 import { statusMessages } from "../config.js";
+
+import { validateCartSchema } from "../schemas/cart.js";
 
 export async function getCart(req, res) {
   const id = req.session;
@@ -18,12 +23,61 @@ export async function getCart(req, res) {
     return res.json({
       status: statusMessages.success,
       message: "Cart found",
-      cart,
+      cart: cart.cart,
     });
   } catch (_) {
     return res.status(500).json({
       status: statusMessages.error,
       message: "An error occurred while finding the cart",
+    });
+  }
+}
+
+export async function putCart(req, res) {
+  const { data, error } = validateCartSchema(req.body);
+
+  if (error) {
+    return res.status(422).json({
+      status: statusMessages.error,
+      message: error.errors[0].message,
+    });
+  }
+
+  const id = req.session;
+
+  try {
+    const productIds = data.map(({ productId }) =>
+      Types.ObjectId.createFromHexString(productId)
+    );
+
+    const existingProducts = await Product.find({ _id: { $in: productIds } });
+
+    if (existingProducts.length !== productIds.length) {
+      return res.status(404).json({
+        status: statusMessages.error,
+        message: "Some products don't exist",
+      });
+    }
+
+    const parsedCart = data.map(({ productId, quantity }) => ({
+      productId: Types.ObjectId.createFromHexString(productId),
+      quantity,
+    }));
+
+    await Cart.findOneAndUpdate(
+      { userId: id },
+      { cart: parsedCart, userId: id },
+      { upsert: true }
+    );
+
+    return res.json({
+      status: statusMessages.success,
+      message: "Cart updated",
+    });
+  } catch (_) {
+    return res.status(500).json({
+      status: statusMessages.error,
+      message: "An error occurred while editing the cart",
     });
   }
 }
